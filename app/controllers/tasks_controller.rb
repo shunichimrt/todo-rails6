043c164161ffea3f3473b18e3009be3701fb2d6a
shunichimrt <smurata.official@gmail.com>
2022-05-11 00:47:01 +0900

    add #destroy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,35 +1,35 @@
 class TasksController < ApplicationController
     def index
-      @tasks = Task.all
+        @tasks = Task.all
     end
-  
+
     def new
-      @task = Task.new
+        @task = Task.new
     end
-  
+
     def create
-      @task = Task.create(task_params)
-      redirect_to tasks_path
+        @task = Task.create(task_params)
+        redirect_to tasks_path
     end
-  
+
     def edit
-      @task = Task.find(params[:id])
+        @task = Task.find(params[:id])
     end
-  
+
     def update
-      @task = Task.find(params[:id])
-      @task.update(task_params)
-      redirect_to tasks_path
+        @task = Task.find(params[:id])
+        @task.update(task_params)
+        redirect_to tasks_path
     end
-  
+
     def destroy
-      @task = Task.find(params[:id])
-      @task.destroy
-      redirect_to tasks_path
+        @task = Task.find(params[:id])
+        @task.destroy
+        redirect_to tasks_path
     end
-  
+
     private
-      def task_params
-        params.require(:task).permit(:title)
-      end
-  end
+        def task_params
+            params.require(:task).permit(:title)
+        end
+end
