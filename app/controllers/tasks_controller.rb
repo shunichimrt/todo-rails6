ae8558ae96bdc6cf0c53b3a99fc014ea2ce6d5e9
shunichimrt <smurata.official@gmail.com>
2022-05-10 19:28:20 +0900

    create simple todo

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,35 @@
 class TasksController < ApplicationController
     def index
-        @tasks = Task.all
+      @tasks = Task.all
     end
-end
+  
+    def new
+      @task = Task.new
+    end
+  
+    def create
+      @task = Task.create(task_params)
+      redirect_to tasks_path
+    end
+  
+    def edit
+      @task = Task.find(params[:id])
+    end
+  
+    def update
+      @task = Task.find(params[:id])
+      @task.update(task_params)
+      redirect_to tasks_path
+    end
+  
+    def destroy
+      @task = Task.find(params[:id])
+      @task.destroy
+      redirect_to tasks_path
+    end
+  
+    private
+      def task_params
+        params.require(:task).permit(:title)
+      end
+  end
